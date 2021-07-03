35755e608c46f9c3796fb875f7c7f704fb9b71f4
Omar Ansari <omar.ansari.wt@gmail.com>
2021-07-03 13:27:05 +0200

    Add springboot starter web dependency

---
+++ src/main/kotlin/com/up/features/FeaturesApplication.kt
@@ -0,0 +1,11 @@
+package com.up.features
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FeaturesApplication
+
+fun main(args: Array<String>) {
+	runApplication<FeaturesApplication>(*args)
+}
---
+++ src/test/kotlin/com/up/features/FeaturesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.up.features
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FeaturesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
