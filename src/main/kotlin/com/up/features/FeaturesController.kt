98e4c2ea3477178c0c827064351fe33b0118d532
Omar Ansari <omar.ansari.wt@gmail.com>
2021-07-03 15:26:31 +0200

    Remove redundant imports

--- src/main/kotlin/com/up/features/FeaturesController.kt
+++ src/main/kotlin/com/up/features/FeaturesController.kt
@@ -1,8 +1,5 @@
-import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
