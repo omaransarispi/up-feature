06d3ad6edf44bb06bc340c3f515145f2eaa8cd30
Omar Ansari <omar.ansari.wt@gmail.com>
2021-07-06 02:08:31 +0200

    Extend test with expected features response

--- src/main/kotlin/com/up/features/FeaturesController.kt
+++ src/main/kotlin/com/up/features/FeaturesController.kt
@@ -1,10 +1,34 @@
+package com.up.features
+
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.sql.Timestamp
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 @RestController
-@RequestMapping("/features", headers=["Accept=application/json"])
+@RequestMapping("/features", headers = ["Accept=*/*"])
 class FeaturesController {
-  @GetMapping
-  fun list(){}
+    @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE], consumes = [MediaType.ALL_VALUE])
+    fun list(): List<FeatureResponse> {
+        return listOf(
+            FeatureResponse(
+                "id",
+                Timestamp.valueOf(LocalDate.now().atStartOfDay()),
+                LocalDate.now().atStartOfDay(),
+                LocalDate.now().atStartOfDay().plusHours(1),
+                "Sentinel-1B"
+            )
+        )
+    }
 }
+
+data class FeatureResponse(
+    val id: String,
+    val timestamp: Timestamp,
+    val beginViewingDate: LocalDateTime,
+    val endViewingDate: LocalDateTime,
+    val missionName: String
+)
--- src/test/kotlin/com/up/features/FeaturesControllerTest.kt
+++ src/test/kotlin/com/up/features/FeaturesControllerTest.kt
@@ -1,27 +1,29 @@
 package com.up.features
 
-import FeaturesController
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.MediaType
-import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 
 
-@SpringBootTest(classes = [FeaturesController::class])
-@ActiveProfiles("test")
-@AutoConfigureMockMvc
-class FeaturesControllerTest {
-
-    @Autowired
-    private lateinit var mvc: MockMvc
-
+@WebMvcTest
+class FeaturesControllerTest(@Autowired val mvc: MockMvc) {
     @Test
-    fun `returns an OK response when list is requested`() {
-        mvc.get("/features"){contentType=MediaType.APPLICATION_JSON}.andExpect {
+    fun `returns a collection of features when list is requested`() {
+        mvc.get("/features") { accept = MediaType.APPLICATION_JSON }.andExpect {
+            val expectedResponse = """
+                [{
+                    "id":"id",
+                    "timestamp":"2021-07-05T22:00:00.000+00:00",
+                    "beginViewingDate":"2021-07-06T00:00:00",
+                    "endViewingDate":"2021-07-06T01:00:00",
+                    "missionName":"Sentinel-1B"
+                }]
+            """.trimIndent()
+            content { contentType(MediaType.APPLICATION_JSON) }
+            content { json(expectedResponse) }
             status { isOk() }
         }
     }
